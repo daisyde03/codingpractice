7c637ac932a10daf85936d1b4e9fcdd3b41e2c1c
daisyde03 <daisyde03@gmail.com>
2022-09-10 14:23:25 +0530

    bitmagazine1009

--- bitMagazine.py
+++ bitMagazine.py
@@ -1,13 +1,13 @@
 #problem statement
-#https://www.codechef.com/submit/TRMAG?
+#https://www.codechef.com/submit/TRMAG?jh
 
 for _ in range(int(input())):
-    n=int(input())
-    l=list(map(int,input().split()))
-    x=(n*(n+1))/2
-    for i in range(1,l[0]+1):
-        x=x-l[i]
-    f=int(input())
+    n = int(input())
+    l = list(map(int, input().split()))
+    x = (n*(n+1))/2
+    for i in range(1, l[0]+1):
+        x= x-l[i]
+    f = int(input())
     n = int((n + 1) / 2)
-    x=x*(n-f)/n
-    print("%.4f"%x)
+    x = x*(n-f)/n
+    print("%.4f" %x)
